9ce8ced5b5c15022d5d43ff815c49a5388b8e6da
Tran Tuan <chenjun6403@163.com>
2021-12-26 16:55:31 +0800

    双相机并行测试

--- main.cpp
+++ main.cpp
@@ -8,57 +8,54 @@
 #include "modules/camera-base/camera_factory.h"
 
 int main() {
-    auto *cam = CF_CREATE_CAMERA("DHCamera");
+    auto *hik_cam = CF_CREATE_CAMERA("HikCamera");
+    auto *dh_cam = CF_CREATE_CAMERA("DHCamera");
 
     el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
     el::Loggers::addFlag(el::LoggingFlag::MultiLoggerSupport);
 
     LOG(INFO) << "Waiting for camera...";
 
-    while (!cam->OpenCamera("KE0210010102", "../config/cameras/KE0210010102.txt"))
+    while (!hik_cam->OpenCamera("00D59818552", "../config/cameras/MV-CA003-21UC_00D59818552.mfs"))
         sleep(1);
 
-    cam->StartStream();
-    Frame frame;
-    uint32_t frame_count = 0;
-    uint32_t last_second_frame_cont = 0;
-    time_t start_time = time(nullptr), end_time;
-    for (uint32_t fps = 0; cv::waitKey(1) != 'q'; ++frame_count) {
-        frame.image.release();
-        if (!cam->GetFrame(frame)) {
-            --frame_count;
-            continue;
-        }
-        cv::putText(frame.image,
-                    "FRAME:" + std::to_string(frame_count) + " FPS: " + std::to_string(fps),
-                    cv::Point(0, 24),
-                    cv::FONT_HERSHEY_DUPLEX,
-                    1,
-                    cv::Scalar(0, 255, 0),
-                    1,
-                    false);
+    while (!dh_cam->OpenCamera("KE0210010102", "../config/cameras/KE0210010102.txt"))
+        sleep(1);
 
-        cv::putText(frame.image,
-                    "TIME STAMP: " + std::to_string(frame.time_stamp),
-                    cv::Point(0, 48),
-                    cv::FONT_HERSHEY_DUPLEX,
-                    1,
-                    cv::Scalar(0, 255, 0),
-                    1,
-                    false);
-        cv::imshow("CAM", frame.image);
-        end_time = time(nullptr);
-        if (end_time - start_time == 1) {
-            fps = frame_count - last_second_frame_cont;
-            last_second_frame_cont = frame_count;
-            start_time = time(nullptr);
-        } else if (end_time - start_time > 1) {
-            last_second_frame_cont = frame_count;
-            start_time = time(nullptr);
+    hik_cam->StartStream();
+    dh_cam->StartStream();
+    Frame hik_frame, dh_frame;
+    do {
+        if (hik_cam->GetFrame(hik_frame)) {
+            cv::putText(hik_frame.image,
+                        "TIME STAMP: " + std::to_string(hik_frame.time_stamp),
+                        cv::Point(0, 24),
+                        cv::FONT_HERSHEY_DUPLEX,
+                        1,
+                        cv::Scalar(0, 255, 0),
+                        1,
+                        false);
+            cv::imshow("HikCam", hik_frame.image);
         }
-    }
-    frame.image.release();
-    cam->StopStream();
-    cam->CloseCamera();
+
+        if (dh_cam->GetFrame(dh_frame)) {
+            cv::putText(dh_frame.image,
+                        "TIME STAMP: " + std::to_string(dh_frame.time_stamp),
+                        cv::Point(0, 24),
+                        cv::FONT_HERSHEY_DUPLEX,
+                        1,
+                        cv::Scalar(0, 255, 0),
+                        1,
+                        false);
+            cv::imshow("DHCam", dh_frame.image);
+        }
+    } while (cv::waitKey(1) != 'q');
+    hik_frame.image.release();
+    hik_cam->StopStream();
+    hik_cam->CloseCamera();
+
+    dh_frame.image.release();
+    dh_cam->StopStream();
+    dh_cam->CloseCamera();
     return 0;
 }
